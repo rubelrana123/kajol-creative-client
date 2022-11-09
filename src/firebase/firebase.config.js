2207cd71823d4f13408a52300f65ca9607a5b70d
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-11-10 04:13:10 +0600

    hide firebase info with .env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSsHLxC9q94MWMhk6KFUGjwGFBbbD2NRQ",
-  authDomain: "kajolcreative.firebaseapp.com",
-  projectId: "kajolcreative",
-  storageBucket: "kajolcreative.appspot.com",
-  messagingSenderId: "851502546674",
-  appId: "1:851502546674:web:e1057486484983085a5a6b"
+  apiKey:process.env.REACT_APP_apiKey ,
+  authDomain:process.env.REACT_APP_authDomain ,
+  projectId:process.env.REACT_APP_projectId ,
+  storageBucket:process.env.REACT_APP_storageBucket ,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId ,
+  appId:process.env.REACT_APP_appId 
 };
 
 // Initialize Firebase
